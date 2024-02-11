595b68875a5fb3af96c2a4a94ea407094a204cfa
firoshamhd <135963904+firoshamhd@users.noreply.github.com>
2024-02-11 22:10:48 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,62 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from openai import OpenAI
+import asyncio
+
+loop = asyncio.get_event_loop()
+
+SUPPORT_GROUP = -1001885126003
+SUPPORT_LINK = 'https://t.me/chatgptbotsupport'
+OPENAI_API = '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'
+
+START_MESSAGE = """ʜᴇʟʟᴏ, {user},
+ᴍʏ ɴᴀᴍᴇ ɪꜱ ᴀɪ ʙᴏᴛ
+ɪ ᴀᴍ ᴀɴ ᴀɪ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ꜰɪʀᴏꜱ
+ʏᴏᴜ ᴄᴀɴ ᴀꜱᴋ ᴍᴇ qᴜᴇꜱᴛɪᴏɴ ʙʏ ᴩʟᴀᴄɪɴɢ ʏᴏᴜʀ qᴜᴇꜱᴛɪᴏɴ ᴀꜰᴛᴇʀ /openai ᴄᴏᴍᴍᴀɴᴅ
+ɴᴏᴡ ᴀꜱᴋ ʏᴏᴜʀ qᴜᴇꜱᴛɪᴏɴ ?
+ᴇxᴀᴍᴩʟᴇ : /openai who invented Ai ?"""
+API_ID =17432758
+API_HASH = 'c9e31fda0ce722e3f3033a9d4f140783'
+TOKEN = '6714298301:AAH-oqtHBLQXF2cLJqYHVz1zLGe3PeF7RLU'
+
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+
+# AI setup
+ai_client = OpenAI(api_key=OPENAI_API)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    welcome_message = START_MESSAGE.format(user=message.from_user.first_name)
+    btn1 = [[
+            InlineKeyboardButton('Support Group', url=SUPPORT_LINK),
+            InlineKeyboardButton('ADMIN',url='https://t.me/firossha')
+        ]]
+    await message.reply_text(welcome_message,reply_markup=InlineKeyboardMarkup(btn1))
+
+@app.on_message(filters.command("openai"))
+async def ask_question(client, message):
+    if len(OPENAI_API) == 0:
+        return await message.reply("OPENAI_API is empty")
+    if message.chat.id != SUPPORT_GROUP:
+        btn = [[
+            InlineKeyboardButton('Support Group', url=SUPPORT_LINK)
+        ]]
+        return await message.reply("This command only working in support group.", reply_markup=InlineKeyboardMarkup(btn))
+    try:
+        text = message.text.split(" ", 1)[1]
+    except:
+        return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
+    stk = await message.reply_sticker("CAACAgUAAxkBAAIYJ2XA-H0GrHrCCetu4hHCN-3rss_EAAJfAQAC5k-5VFd6fFm2UUXSHgQ")
+    try:
+        response = ai_client.chat.completions.create(
+            messages=[
+                {"role": "user", "content": text}
+            ],
+            model="gpt-3.5-turbo"
+        )
+        await message.reply(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].message.content}</code>")
+        return await stk.delete()
+    except Exception as e:
+        await msg.edit(f'Error - <code>{e}</code>')
+        return await stk.delete()
+app.run()
